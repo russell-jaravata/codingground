fa9ab738de38be611ae0d8a1238c2e6cd9ac8494
russell-jaravata <russell.jaravata@gmail.com>
2016-05-04 05:32:54 +0800

    Update Comparison

--- Bash for Memory Check/main.sh
+++ Bash for Memory Check/main.sh
@@ -5,11 +5,11 @@ USED_MEMORY=`free | grep Mem: | awk '{ print $3 }'`
 echo \"used = $USED_MEMORY\"
 myvar=`echo "$USED_MEMORY $TOTAL_MEMORY" | awk '{printf "%.3f \n", $1/$2}'`
 echo $myvar
-declare -i myvar2
-myvar2=`echo "$myvar 100" | awk '{printf "%.0f \n", $1*$2}'`
-echo myvar2 $myvar2
-let myvar3=$myvar2+1
-echo myvar3 \"$myvar3\"
+declare -i USED
+USED=`echo "$myvar 100" | awk '{printf "%.0f \n", $1*$2}'`
+echo %USED= $USED
+#let myvar3=$USED+1
+#echo myvar3 \"$myvar3\"
 #myvar2=`echo - | awk '{print $USED_MEMORY / $TOTAL_MEMORY}'`
 #myvar3=`awk 'BEGIN {printf "%.3f\n", $USED_MEMORY/$TOTAL_MEMORY}'`
 #RESULT=$(awk "BEGIN {printf \"%.2f\",${USED_MEMORY}/${TOTAL_MEMORY}}")
@@ -17,8 +17,11 @@ echo myvar3 \"$myvar3\"
 #echo $myvar3
 #echo "Hello World!" 
 
+declare -i CRIT
+declare -i WARN
+
 if [ $# -eq 0 ]; then
-	echo "Script requires 3 parameters: -c critical threshold(percentage) -w warning threshold(percentage) -e email address to send the report"
+	echo "No parameters. Script requires 3 parameters: -c critical threshold(percentage) -w warning threshold(percentage) -e email address to send the report"
 else 
 	while getopts ":c:e:w:" opt; do
   case $opt in
@@ -44,9 +47,26 @@ else
       ;;
   esac
 done
-if [ CRIT -le WARN ]; then
-	echo "Script requires 3 parameters: -c critical threshold(percentage) -w warning threshold(percentage) -e email address to send the report"
+if [ $CRIT -le $WARN ]; then
+	echo "Critical Threshold is less than Warning Threshold. Script requires 3 parameters: -c critical threshold(percentage) -w warning threshold(percentage) -e email address to send the report"
 else 
    echo "compute"
+   if [ $USED -lt $WARN ] ; then
+        echo "Used Memory is less than Warning Threshold."
+        exit 0
+    else      
+        if [ $USED -lt $CRIT ] && [ $USED -ge $WARN ] ; then
+                 echo "Used Memory is less than Critical Threshold but greater than or equal to Warning Threshold."
+                 exit 1
+        else
+                if [ $USED -ge $CRIT ] ; then
+                        echo "Used Memory is greater than or equal to Critical Threshold."
+                        
+                        exit 2
+                #else
+                 #       echo "Please make a choice between 1-3 !"
+                fi   
+        fi
+    fi
 fi
 fi 
