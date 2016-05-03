9d94d38d12bd0d7db5698ad9dff22db8a6ce2445
russell-jaravata <russell.jaravata@gmail.com>
2016-05-04 06:47:49 +0800

    Update Code without Sending Email

--- Bash for Memory Check/main.sh
+++ Bash for Memory Check/main.sh
@@ -1,13 +1,5 @@
 # Hello World Program in Bash Shell
-TOTAL_MEMORY=`free | grep Mem: | awk '{ print $2 }'`
-echo \"total = $TOTAL_MEMORY\"
-USED_MEMORY=`free | grep Mem: | awk '{ print $3 }'`
-echo \"used = $USED_MEMORY\"
-myvar=`echo "$USED_MEMORY $TOTAL_MEMORY" | awk '{printf "%.3f \n", $1/$2}'`
-echo $myvar
-declare -i USED
-USED=`echo "$myvar 100" | awk '{printf "%.0f \n", $1*$2}'`
-echo %USED= $USED
+
 #let myvar3=$USED+1
 #echo myvar3 \"$myvar3\"
 #myvar2=`echo - | awk '{print $USED_MEMORY / $TOTAL_MEMORY}'`
@@ -19,9 +11,6 @@ echo %USED= $USED
 #now="$(date)"
 #printf "Current date and time %s\n" "$now"
 
-
-
-
 declare -i CRIT
 declare -i WARN
 
@@ -32,15 +21,15 @@ else
   case $opt in
     c)
       CRIT=$OPTARG
-      echo "-c was triggered, Parameter: $OPTARG" >&2
+      #echo "-c was triggered, Parameter: $OPTARG" >&2
       ;;
     w)
       WARN=$OPTARG
-      echo "-w was triggered, Parameter: $OPTARG" >&2
+      #echo "-w was triggered, Parameter: $OPTARG" >&2
       ;; 
     e)
       EMAIL=$OPTARG
-      echo "-e was triggered, Parameter: $OPTARG" >&2
+      #echo "-e was triggered, Parameter: $OPTARG" >&2
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -55,7 +44,16 @@ done
 if [ $CRIT -le $WARN ]; then
 	echo "Critical Threshold is less than Warning Threshold. Script requires 3 parameters: -c critical threshold(percentage) -w warning threshold(percentage) -e email address to send the report"
 else 
-   echo "compute"
+   #echo "compute"
+   TOTAL_MEMORY=`free | grep Mem: | awk '{ print $2 }'`
+   echo \"total = $TOTAL_MEMORY\"
+   USED_MEMORY=`free | grep Mem: | awk '{ print $3 }'`
+   echo \"used = $USED_MEMORY\"
+   myvar=`echo "$USED_MEMORY $TOTAL_MEMORY" | awk '{printf "%.3f \n", $1/$2}'`
+   #echo $myvar
+   declare -i USED
+   USED=`echo "$myvar 100" | awk '{printf "%.0f \n", $1*$2}'`
+   echo %USED= $USED
    if [ $USED -lt $WARN ] ; then
         echo "Used Memory is less than Warning Threshold."
         exit 0
@@ -68,14 +66,12 @@ else
                         echo "Used Memory is greater than or equal to Critical Threshold."
                         SUBJECT="$(date +'%Y%m%d %R') memory check -critical"
                         echo $SUBJECT
-                        mail -s $SUBJECT russell.jaravata@gmail.com <<< "message"
-                        mail -s $SUBJECT russell.jaravata@gmail.com < /dev/null
-                        mutt -s $SUBJECT russell.jaravata@gmail.com < /dev/null
+                        #mail -s $SUBJECT russell.jaravata@gmail.com <<< "message"
+                        #mail -s $SUBJECT russell.jaravata@gmail.com < /dev/null
+                        #mutt -s $SUBJECT russell.jaravata@gmail.com < /dev/null
                         exit 2
-                #else
-                 #       echo "Please make a choice between 1-3 !"
                 fi   
         fi
     fi
 fi
-fi 
+fi
